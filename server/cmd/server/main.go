f995a385f034f830bec1ce71d8e00450491c46d1
Tyree Carpenter <carpentertd@vcu.edu>
2025-09-22 14:27:54 -0400

    changed everything around & added ci/cd

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"shithead/internal/httpserver"
+	"shithead/internal/ws"
+)
+
+func main() {
+	http.HandleFunc("/ws", ws.Handler)
+	http.HandleFunc("/", httpserver.Handler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
